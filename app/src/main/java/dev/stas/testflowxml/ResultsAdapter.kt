bdb36afa13b71d3b944ed75b5a468c24e80136cc
Leonov Stanislav <ragioniere1980@gmail.com>
2024-06-21 17:23:18 +0300

    Initial commit

--- app/src/main/java/dev/stas/testflowxml/ResultsAdapter.kt
+++ app/src/main/java/dev/stas/testflowxml/ResultsAdapter.kt
@@ -19,21 +19,39 @@ class ResultsAdapter : ListAdapter<Pair<Int, List<Int>>, ResultsAdapter.ResultVi
         holder.bind(inputValue, resultList)
     }
 
+    override fun onBindViewHolder(holder: ResultViewHolder, position: Int, payloads: MutableList<Any>) {
+        if (payloads.isEmpty()) {
+            super.onBindViewHolder(holder, position, payloads)
+        } else {
+            val payload = payloads[0] as List<Int>
+            holder.updateResults(payload)
+        }
+    }
+
     class ResultViewHolder(private val binding: ItemResultBinding) : RecyclerView.ViewHolder(binding.root) {
+
         fun bind(inputValue: Int, resultList: List<Int>) {
             binding.inputValueTextView.text = "Введенное количество: $inputValue"
             binding.resultTextView.text = "Результат: ${resultList.joinToString(" ")}"
+        }
+
+        fun updateResults(updatedResultList: List<Int>) {
+            binding.resultTextView.text = "Результат: ${updatedResultList.joinToString(" ")}"
         }
     }
 
     class ResultsDiffCallback : DiffUtil.ItemCallback<Pair<Int, List<Int>>>() {
 
         override fun areItemsTheSame(oldItem: Pair<Int, List<Int>>, newItem: Pair<Int, List<Int>>): Boolean {
-            return oldItem == newItem
+            return oldItem.first == newItem.first
         }
 
         override fun areContentsTheSame(oldItem: Pair<Int, List<Int>>, newItem: Pair<Int, List<Int>>): Boolean {
             return oldItem == newItem
+        }
+
+        override fun getChangePayload(oldItem: Pair<Int, List<Int>>, newItem: Pair<Int, List<Int>>): Any? {
+            return if (oldItem.second != newItem.second) newItem.second else null
         }
     }
 }
